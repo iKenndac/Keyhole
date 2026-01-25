aa9ee579cc8e4f00e6ff9442f663ce89a30a6111
Graham Hall <graham@ghall.dev>
2026-01-25 08:07:41 -0500

    Add Doppler integration (#3)
    * Add Doppler integration
    
    * Add entitlement for Doppler
    
    * add Doppler to README

---
+++ Keyhole/App Integrations/Doppler App Integration/DopplerAppIntegration.swift
@@ -0,0 +1,21 @@
+import Foundation
+import Observation
+import ScriptingBridge
+
+@MainActor @Observable class DopplerAppIntegration: ScriptableAppIntegration<DopplerApplication> {
+	
+	override class var bundleId: String { return "co.brushedtype.doppler-macos" }
+	override class var appName: String { return "Doppler" } // Localised?
+	
+	override func playPause() throws(MediaAppCommandError) {
+		try scriptableApp().playpause?()
+	}
+	
+	override func skipBack() throws(MediaAppCommandError) {
+		try scriptableApp().previousTrack?()
+	}
+	
+	override func skipForward() throws(MediaAppCommandError) {
+		try scriptableApp().nextTrack?()
+	}
+}
